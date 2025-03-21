36dd33c7b5a63c8c970e03109c671531d9dca7bb
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-03-21 15:33:20 +0330

    Add CORS options middleware for handling cross-origin requests

---
+++ src/middlewares/corsOptions.js
@@ -0,0 +1,5 @@
+exports.corseOptions = {
+  origin: "*", //  Access-Control-Allow-Origin: "*"
+  methods: ["GET", "POST", "PUT", "DELETE"], //  Access-Control-Allow-Methods
+  allowedHeaders: ["Content-Type", "Authorization"], //  Access-Control-Allow-Headers
+};
