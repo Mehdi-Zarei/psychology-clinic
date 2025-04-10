79594fea788e5fc257b8d45df9ab9fd1ee7b591b
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-04-10 21:39:09 +0330

    Refactor authentication controller to enhance OTP handling, user registration, and login processes

--- src/modules/auth/v1/auth.controller.js
+++ src/modules/auth/v1/auth.controller.js
@@ -1,5 +1,19 @@
+//* Bcrypt Helper
+const { hashData, comparHashedData } = require("../../../helper/bcrypt");
+
+//* jwt Helper
+const {
+  generateAccessToken,
+  generateRefreshToken,
+} = require("../../../helper/jwtTokens");
+
 //* Redis Helpers
-const { getOtpInfo, saveOtp, removeOtp } = require("../../../helper/redis");
+const {
+  getOtpInfo,
+  saveData,
+  removeData,
+  getData,
+} = require("../../../helper/redis");
 
 //* Response Messages
 const {
@@ -10,7 +24,7 @@ const {
 //* Services
 const sentSms = require("../../../service/otp");
 
-const userModel = require("./auth.model");
+const userModel = require("../../../model/User");
 
 exports.sentOtp = async (req, res, next) => {
   try {
@@ -35,9 +49,7 @@ exports.sentOtp = async (req, res, next) => {
     const generateOtpCode =
       Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000;
 
-    console.log(generateOtpCode); //TODO: Remove log
-
-    await saveOtp(phone, generateOtpCode);
+    await saveData(`otp:${phone}`, generateOtpCode, 60);
 
     const smsResult = await sentSms(phone, generateOtpCode);
     if (smsResult.success) {
@@ -47,7 +59,7 @@ exports.sentOtp = async (req, res, next) => {
         "کد ورود به شماره موبایل وارد شده باموفقیت ارسال گردید."
       );
     } else {
-      await removeOtp(phone);
+      await removeData(`otp:${phone}`);
 
       return errorResponse(
         res,
@@ -62,6 +74,51 @@ exports.sentOtp = async (req, res, next) => {
 
 exports.verify = async (req, res, next) => {
   try {
+    const { otpCode, phone } = req.body;
+
+    const storedOtp = await getData(`otp:${phone}`);
+
+    if (storedOtp.expired || storedOtp !== otpCode) {
+      return errorResponse(
+        res,
+        403,
+        "کد وارد شده صحیح نمی باشد و یا مدت زمان آن به پایان رسیده است."
+      );
+    }
+
+    const isUserExist = await userModel.findOne({ phone }).lean();
+
+    if (isUserExist) {
+      if (isUserExist.isRestrict) {
+        return errorResponse(res, 403, "حساب کاربری شما مسدود شده است.");
+      }
+
+      const accessToken = await generateAccessToken(
+        isUserExist._id,
+        isUserExist.role
+      );
+
+      const refreshToken = await generateRefreshToken(isUserExist._id);
+
+      const encryptRefreshToken = await hashData(refreshToken);
+
+      await saveData(
+        `refreshToken:${isUserExist._id}`,
+        encryptRefreshToken,
+        2592000
+      );
+
+      await removeData(`otp:${phone}`);
+
+      return successResponse(
+        res,
+        200,
+        "شما با موفقیت وارد حساب کاربری خود شدید.",
+        { accessToken, refreshToken }
+      );
+    } else {
+      return errorResponse(res, 404, "Redirect To Register Page.");
+    }
   } catch (error) {
     next(error);
   }
@@ -69,6 +126,47 @@ exports.verify = async (req, res, next) => {
 
 exports.register = async (req, res, next) => {
   try {
+    const { name, email, phone, password } = req.body;
+
+    const isUserExist = await userModel
+      .findOne({ $or: [{ email }, { phone }] })
+      .lean();
+
+    if (isUserExist) {
+      return errorResponse(
+        res,
+        409,
+        "این آدرس ایمیل و یا شماره موبایل تکراری است."
+      );
+    }
+
+    const hashPassword = password ? await hashData(password) : password;
+
+    console.log("hashPassword->", hashPassword);
+
+    const isFirstUser = (await userModel.countDocuments()) === 0;
+
+    const newUser = await userModel.create({
+      name,
+      email,
+      phone,
+      password: hashPassword,
+      role: isFirstUser ? "ADMIN" : "USER",
+      isRestrict: false,
+    });
+
+    const accessToken = await generateAccessToken(newUser._id, newUser.role);
+
+    const refreshToken = await generateRefreshToken(newUser._id);
+
+    const encryptRefreshToken = await hashData(refreshToken);
+
+    await saveData(`refreshToken:${newUser._id}`, encryptRefreshToken, 2592000);
+
+    return successResponse(res, 201, "ثبت نام با موفقیت انجام شد.", {
+      accessToken,
+      refreshToken,
+    });
   } catch (error) {
     next(error);
   }
@@ -76,13 +174,45 @@ exports.register = async (req, res, next) => {
 
 exports.login = async (req, res, next) => {
   try {
-  } catch (error) {
-    next(error);
-  }
-};
+    const { identifier, password } = req.body;
 
-exports.loginWithOtp = async (req, res, next) => {
-  try {
+    const user = await userModel
+      .findOne({ $or: [{ email: identifier }, { phone: identifier }] })
+      .lean();
+
+    if (!user) {
+      return errorResponse(res, 404, "حساب کاربری پیدا نشد.");
+    }
+
+    if (user.isRestrict) {
+      return errorResponse(res, 403, "حساب کاربری شما مسدود شده است.");
+    }
+
+    if (!user.password) {
+      return errorResponse(
+        res,
+        403,
+        "برای ورود ابتدا باید رمز عبور تنظیم کنید. لطفاً از طریق کد یکبارمصرف وارد حساب کاربری خود شویدو از طریق پروفایل خود یک رمز عبور تعیین کنید."
+      );
+    }
+
+    const verifyPassword = await comparHashedData(password, user.password);
+
+    if (!verifyPassword) {
+      return errorResponse(res, 403, "نام کاربری و یا رمز عبور صحیح نمی باشد.");
+    }
+
+    const accessToken = await generateAccessToken(user._id, user.role);
+    const refreshToken = await generateRefreshToken(user._id);
+    const hashRefreshToken = await hashData(refreshToken);
+    await saveData(`refreshToken:${user._id}`, hashRefreshToken, 2592000);
+
+    return successResponse(
+      res,
+      200,
+      "شما با موفقیت وارد حساب کاربری خودتون شدید.",
+      { accessToken, refreshToken }
+    );
   } catch (error) {
     next(error);
   }
@@ -90,6 +220,21 @@ exports.loginWithOtp = async (req, res, next) => {
 
 exports.logout = async (req, res, next) => {
   try {
+    const user = req.user;
+
+    await removeData(`refreshToken:${user._id}`);
+
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "Strict",
+    });
+
+    return successResponse(
+      res,
+      200,
+      "شما با موفقیت از حساب کاربری خود خارج شدید."
+    );
   } catch (error) {
     next(error);
   }
@@ -97,13 +242,21 @@ exports.logout = async (req, res, next) => {
 
 exports.refreshAccessToken = async (req, res, next) => {
   try {
+    const user = req.user;
+
+    const accessToken = await generateAccessToken(user._id, user.role);
+
+    return successResponse(res, 200, accessToken);
   } catch (error) {
     next(error);
   }
 };
 
-exports.gtMe = async (req, res, next) => {
+exports.getMe = async (req, res, next) => {
   try {
+    const user = req.user;
+
+    return successResponse(res, 200, user);
   } catch (error) {
     next(error);
   }
