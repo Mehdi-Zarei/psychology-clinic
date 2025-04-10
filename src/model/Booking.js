a4cbee88e8e83321c6401096d453aa1d95b97dec
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-04-10 21:38:52 +0330

    Add Booking model for user and psychologist appointments

---
+++ src/model/Booking.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    psychologist: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["reserved", "canceled", "done"],
+      default: "reserved",
+    },
+  },
+  { timestamps: true }
+);
+
+const booking = mongoose.model("booking", bookingSchema);
+
+module.exports = booking;
