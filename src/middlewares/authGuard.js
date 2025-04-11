b68dacbc2dc1ae5f46db963e2d7a06f57703b8b1
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-04-11 16:42:44 +0330

    Refactor authGuard middleware to support role-based access control and improve error handling

--- src/middlewares/authGuard.js
+++ src/middlewares/authGuard.js
@@ -1,29 +1,43 @@
 const userModel = require("../model/User");
 const { decodeToken } = require("../helper/jwtTokens");
 
-const authGuard = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization?.split(" ")[1];
+const authGuard = (requiredRoles = []) => {
+  return async (req, res, next) => {
+    try {
+      const token = req.headers.authorization?.split(" ")[1];
 
-    if (!token) {
-      return res.status(401).json({
-        message: "لطفا ابتدا وارد حساب کاربری خود شوید.",
-      });
+      if (!token) {
+        return res.status(401).json({
+          message: "لطفاً ابتدا وارد حساب کاربری خود شوید.",
+        });
+      }
+
+      const decoded = await decodeToken(token);
+
+      const user = await userModel.findById(decoded.id).select("-password");
+
+      if (!user) {
+        return res.status(404).json({ message: "کاربر یافت نشد!" });
+      }
+
+      req.user = user;
+
+      if (requiredRoles.length === 0) {
+        return next();
+      }
+      const hasRequiredRole = requiredRoles.includes(user.role);
+
+      if (!hasRequiredRole) {
+        return res.status(403).json({
+          message: "شما اجازه دسترسی به این مسیر را ندارید.",
+        });
+      }
+
+      next();
+    } catch (error) {
+      next(error);
     }
-
-    const decoded = await decodeToken(token);
-
-    const user = await userModel.findById(decoded.id).select("-password");
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found!" });
-    }
-
-    req.user = user;
-    next();
-  } catch (error) {
-    return res.status(500).json({ message: "Server error", error });
-  }
+  };
 };
 
 module.exports = authGuard;
