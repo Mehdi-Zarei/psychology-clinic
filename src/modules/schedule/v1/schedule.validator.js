f6991712e4416d9a6dc5e30e862c74f1cc587976
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-04-11 21:34:51 +0330

    Add validation schemas for setting available times and editing appointments

---
+++ src/modules/schedule/v1/schedule.validator.js
@@ -0,0 +1,65 @@
+const Joi = require("joi");
+
+const setAvailableTimeSchema = Joi.object({
+  date: Joi.string().isoDate().required().messages({
+    "date.base": "فرمت تاریخ معتبر نیست!",
+    "any.required": "تاریخ ضروری است!",
+    "date.iso": "فرمت تاریخ باید مطابق با استاندارد ISO باشد!",
+  }),
+  availableTimes: Joi.array()
+    .items(
+      Joi.object({
+        time: Joi.string()
+          .pattern(/^([01]([0-9])|2[0-3]):([0-5][0-9])$/)
+          .required()
+          .messages({
+            "string.pattern.base":
+              "فرمت ساعت صحیح نیست! ساعت باید به شکل HH:mm (مثال: 01:00) باشد.",
+            "any.required": "ساعت ضروری است!",
+          }),
+
+        isBooked: Joi.boolean().required().messages({
+          "any.required": "وضعیت رزرو ضروری است!",
+        }),
+      })
+    )
+    .min(1)
+    .required()
+    .messages({
+      "array.min": "حداقل یک تایم باید مشخص شود!",
+      "any.required": "تایم‌های مشاوره ضروری هستند!",
+    }),
+});
+
+const editAppointmentDateSchema = Joi.object({
+  date: Joi.string().isoDate().required().messages({
+    "date.base": "فرمت تاریخ معتبر نیست!",
+    "any.required": "تاریخ ضروری است!",
+    "date.iso": "فرمت تاریخ باید مطابق با استاندارد ISO باشد!",
+  }),
+});
+
+const editAppointmentTimeSchema = Joi.object({
+  newTime: Joi.object({
+    time: Joi.string()
+      .pattern(/^([01]([0-9])|2[0-3]):([0-5][0-9])$/)
+      .required()
+      .messages({
+        "string.pattern.base": "فرمت ساعت صحیح نیست! باید به شکل HH:mm باشد.",
+        "any.required": "ساعت ضروری است!",
+      }),
+    isBooked: Joi.boolean().required().messages({
+      "any.required": "وضعیت رزرو ضروری است!",
+    }),
+  })
+    .required()
+    .messages({
+      "any.required": "مقدار newTime ضروری است!",
+    }),
+});
+
+module.exports = {
+  setAvailableTimeSchema,
+  editAppointmentDateSchema,
+  editAppointmentTimeSchema,
+};
