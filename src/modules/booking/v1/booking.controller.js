3ab25d336b85872dfb65818b6e5ca6743329ceb9
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-04-17 20:08:21 +0330

    Refactor booking controller to improve parameter handling and response structure

--- src/modules/booking/v1/booking.controller.js
+++ src/modules/booking/v1/booking.controller.js
@@ -1,6 +1,26 @@
+const { isValidObjectId } = require("mongoose");
+const { errorResponse } = require("../../../helper/responseMessage");
+const bookingModel = require("../../../model/Booking");
+const scheduleModel = require("../../../model/Schedule");
+
 exports.booking = async (req, res, next) => {
   try {
-    const {} = req.body;
+    const { id } = req.params;
+    const { date, time } = req.body;
+    const user = req.user;
+
+    if (!isValidObjectId(id)) {
+      return errorResponse(res, 409, "آیدی وارد شده صحیح نمی باشد.");
+    }
+
+    const today = new Date();
+    const times = await scheduleModel.find({
+      psychologistID: id,
+      date: { $gte: today },
+      "availableTimes.isBooked": false,
+    });
+
+    return res.json(times);
   } catch (error) {
     next(error);
   }
