8cd3936a191a5ee1ba368c7df98732b6d6ed31d7
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-04-17 20:08:37 +0330

    Refactor psychologist routes to include review management and enhance input validation

--- src/modules/psychologist/v1/psychologist.routes.js
+++ src/modules/psychologist/v1/psychologist.routes.js
@@ -10,22 +10,47 @@ const {
   getAll,
   getOne,
   remove,
+  getReviews,
+  createReview,
+  acceptReview,
+  removeReview,
 } = require("./psychologist.controller");
 
 //* Uploader
 const { multerStorage } = require("../../../utils/multer");
 const upload = multerStorage("public/images/profile", 500, [".jpg", ".jpeg"]);
 
+//* Validator
+const { bodyValidator } = require("../../../middlewares/validator");
+const {
+  psychologistSchema,
+  createReviewSchema,
+} = require("./psychologist.validator");
+
 //* Routes
 
 router
   .route("/")
-  .post(authGuard(["ADMIN"]), upload.single("avatar"), register) //todo body validator
+  .post(
+    authGuard(["ADMIN"]),
+    upload.single("avatar"),
+    bodyValidator(psychologistSchema),
+    register
+  )
   .get(getAll);
 
 router
   .route("/:id")
   .get(getOne)
   .delete(authGuard(["ADMIN"]), remove);
+
+router
+  .route("/:id/reviews")
+  .get(getReviews)
+  .post(authGuard(), bodyValidator(createReviewSchema), createReview);
+
+router.route("/review/:id/accept").patch(authGuard(["ADMIN"]), acceptReview);
+
+router.route("/review/:id/remove").delete(authGuard(["ADMIN"]), removeReview);
 
 module.exports = router;
