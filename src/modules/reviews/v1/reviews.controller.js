3c75c54cb2450708404191d96e3d81d1a36d9489
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-04-20 21:42:34 +0330

    Add reviews controller with CRUD operations for user reviews

---
+++ src/modules/reviews/v1/reviews.controller.js
@@ -0,0 +1,171 @@
+const psychologistModel = require("../../../model/Psychologist");
+
+exports.getUserReviews = async (req, res, next) => {
+  try {
+    const user = req.user;
+
+    const filter =
+      user.role === "ADMIN"
+        ? {}
+        : {
+            reviews: { $elemMatch: { user: user._id } },
+          };
+
+    const allReviews = await psychologistModel
+      .find(filter)
+      .populate("reviews.user", "name")
+      .populate("psychologistID", "name")
+      .select("avatar rating reviews");
+
+    if (!allReviews.length) {
+      return errorResponse(res, 400, "شما تاکنون نظری ثبت نکرده اید.");
+    }
+
+    return successResponse(res, 200, allReviews);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getPsychologistReviews = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!isValidObjectId(id)) {
+      return errorResponse(res, 409, "آیدی وارد شده معتبر نمی باشد.");
+    }
+
+    const psychologist = await psychologistModel
+      .findOne({
+        psychologistID: id,
+      })
+      .populate("reviews.user", "name")
+      .select("reviews");
+
+    if (!psychologist) {
+      return errorResponse(res, 404, "متاسفانه روانشناسی یافت نشد.");
+    }
+
+    if (!psychologist.reviews.length) {
+      return errorResponse(
+        res,
+        404,
+        "برای این روانشناس هنوز نظری ثبت نشده!شما اولین نفری باشید که نظر میدهید."
+      );
+    }
+
+    psychologist.reviews = psychologist.reviews.filter(
+      (review) => review.isAccept === true
+    );
+
+    return successResponse(res, 200, psychologist);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.createReview = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { comment, stars } = req.body;
+    const user = req.user;
+
+    if (!isValidObjectId(id)) {
+      return errorResponse(res, 409, "آیدی وارد شده معتبر نمی باشد.");
+    }
+
+    const psychologist = await psychologistModel.findOne({
+      psychologistID: id,
+    });
+
+    if (!psychologist) {
+      return errorResponse(res, 404, "متاسفانه روانشناس مورد نظر پیدا نشد!");
+    }
+
+    psychologist.reviews.push({
+      user: user._id,
+      comment,
+      stars,
+      isAccept: false,
+    });
+
+    // const totalStars = psychologist.reviews.reduce(
+    //   (sum, review) => sum + review.stars,
+    //   0
+    // );
+
+    // const averageRating = totalStars / psychologist.reviews.length;
+
+    // psychologist.rating = averageRating;
+    await psychologist.save();
+
+    return successResponse(
+      res,
+      201,
+      "نظر شما با موفقیت ثبت شد و پس از تائید مدیر قابل نمایش می باشد."
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.acceptReview = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!isValidObjectId(id)) {
+      return errorResponse(res, 409, "آیدی وارد شده معتبر نمی باشد.");
+    }
+
+    const acceptReview = await psychologistModel.findOneAndUpdate(
+      {
+        "reviews._id": id,
+      },
+      { $set: { "reviews.$.isAccept": true } }
+    );
+
+    if (!acceptReview) {
+      return errorResponse(res, 404, "کامنت یافت نشد!");
+    }
+
+    const totalStars = acceptReview.reviews.reduce(
+      (sum, review) => sum + review.stars,
+      0
+    );
+
+    const averageRating = totalStars / acceptReview.reviews.length.toFixed(1);
+
+    acceptReview.rating = averageRating;
+
+    await acceptReview.save();
+
+    return successResponse(res, 200, "کامنت مورد نظر با موفقیت تائید شد.");
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.removeReview = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!isValidObjectId(id)) {
+      return errorResponse(res, 409, "آیدی وارد شده معتبر نمی باشد.");
+    }
+
+    const review = await psychologistModel.updateOne(
+      {
+        "reviews._id": id,
+      },
+      { $pull: { reviews: { _id: id } } }
+    );
+
+    if (!review.modifiedCount) {
+      return errorResponse(res, 404, "کامنت پیدا نشد.");
+    }
+
+    return successResponse(res, 200, "کامنت مورد نظر با موفقیت پاک شد.");
+  } catch (error) {
+    next(error);
+  }
+};
