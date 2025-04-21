adbc486aaa63f0adcb76a28ea1cf17cd8f290d52
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-04-21 22:16:29 +0330

    Add article routes for CRUD operations and image upload handling

---
+++ src/modules/article/v1/article.routes.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+
+//* Middlewares
+const authGuard = require("../../../middlewares/authGuard");
+
+//* Controller
+const {
+  getAll,
+  create,
+  getOne,
+  edit,
+  remove,
+} = require("./article.controller");
+
+//* Uploader
+const { multerStorage } = require("../../../utils/multer");
+const upload = multerStorage("public/images/articles", 15, [".jpg", ".jpeg"]);
+
+//* Routes
+
+router
+  .route("/")
+  .get(getAll)
+  .post(authGuard("ADMIN"), upload.array("images", 10), create);
+
+router
+  .route("/:id")
+  .get(getOne)
+  .patch(authGuard("ADMIN"), edit)
+  .delete(authGuard("ADMIN"), remove);
+
+module.exports = router;
