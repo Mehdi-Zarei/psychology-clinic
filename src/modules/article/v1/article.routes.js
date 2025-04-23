377e4c50be8d4c2e499222a3844e131cb5689c06
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-04-23 22:53:02 +0330

    Add validation middleware for article creation and editing routes

--- src/modules/article/v1/article.routes.js
+++ src/modules/article/v1/article.routes.js
@@ -19,18 +19,35 @@ const {
 const { multerStorage } = require("../../../utils/multer");
 const upload = multerStorage("public/images/articles", 15, [".jpg", ".jpeg"]);
 
+//* Validator
+const { bodyValidator } = require("../../../middlewares/validator");
+const {
+  createArticleSchema,
+  editArticleSchema,
+} = require("./article.validator");
+
 //* Routes
 
 router
   .route("/")
   .get(getAll)
-  .post(authGuard("ADMIN"), upload.array("images", 10), create);
+  .post(
+    authGuard("ADMIN"),
+    upload.array("images", 10),
+    bodyValidator(createArticleSchema),
+    create
+  );
 
 router.route("/:slug").get(getOne);
 
 router
   .route("/:id")
-  .patch(authGuard("ADMIN"), upload.array("images", 10), edit)
+  .patch(
+    authGuard("ADMIN"),
+    upload.array("images", 10),
+    bodyValidator(editArticleSchema),
+    edit
+  )
   .delete(authGuard("ADMIN"), remove)
   .put(authGuard("ADMIN"), changePublishStatus);
 
