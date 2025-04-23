84f64b0199b2d6159a56933c9d36180421e2ca96
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-04-23 23:07:24 +0330

    Remove validation middleware for booking appointment and cancel booking routes

--- src/modules/booking/v1/booking.routes.js
+++ src/modules/booking/v1/booking.routes.js
@@ -11,26 +11,11 @@ const {
   cancelBooking,
 } = require("./booking.controller");
 
-//* Validator
-const { bodyValidator } = require("../../../middlewares/validator");
-const {
-  bookingAppointmentSchema,
-  cancelBookingSchema,
-} = require("./booking.validator");
-
 //* Routes
 router.route("/").get(authGuard(), getAll);
 
-router
-  .route("/:id")
-  .post(
-    authGuard(),
-    bodyValidator(bookingAppointmentSchema),
-    bookingAppointment
-  );
+router.route("/:id").post(authGuard(), bookingAppointment);
 
-router
-  .route("/:id/cancel")
-  .patch(authGuard(), bodyValidator(cancelBookingSchema), cancelBooking);
+router.route("/:id/cancel").patch(authGuard(), cancelBooking);
 
 module.exports = router;
